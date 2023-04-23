b998f57e7f08e3cbc7d6c7d05a9645efad20535b
Mohamed Amin Boubaker <CodeIter@users.noreply.github.com>
2023-04-23 09:27:40 +0100

    Update slice.sh
    make heneric slices

--- slice.sh
+++ slice.sh
@@ -1,4 +1,29 @@
-#!/bin/bash
-cd minetest_game
-git subtree split -P mods/$1 -b $1
-git push "https://x-access-token:${access_token}@github.com/minetest-game/${1}.git" ${1}:master
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+_git_subtree_branch_prefix="___"
+_git_branch="$(git rev-parse --abbrev-ref HEAD)"
+GITHUB_OWNER="${GITHUB_REPOSITORY%%/*}"
+
+for _i in "${@}" ; do
+
+  [ -e "${_i}" ] || continue
+
+  _dir="${_i}"
+  _basename="$(basename "${_i}")"
+
+  git subtree split -P "${_dir}" -b "${_git_subtree_branch_prefix}${_basename}" \
+  || exit 1
+
+done
+
+readarray -t _git_branches < <(git branch --list "${_git_subtree_branch_prefix}*")
+
+for _i in "${_git_branches}" ; do
+
+  _git_subtree_branch="${_i}"
+  _git_remote_repo_name="${_git_subtree_branch#${_git_subtree_branch_prefix}}"
+  git push "https://x-access-token:${ACCESS_TOKEN}@github.com/${GITHUB_OWNER}/${_git_remote_repo_name}.git" "${_git_subtree_branch}:${_git_branch}"
+
+done
